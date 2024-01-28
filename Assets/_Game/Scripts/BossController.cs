b528673d5e98fa54230870a550f9bbbdf6d04449
Teemu Tammisalo <teemu.tammisalo@avenla.fi>
2024-01-28 11:38:21 +0200

    Made boss bullets hit player and fixed boss spawns not dying

--- Assets/_Game/Scripts/BossController.cs
+++ Assets/_Game/Scripts/BossController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -108,7 +109,7 @@ public class BossController : CharacterBase
 
         if (distanceToTarget < 1f)
         {
-            rBody.velocity = direction * speed * (distanceToTarget / 1f);
+            rBody.velocity = direction * (speed * (distanceToTarget / 1f));
 
             if (distanceToTarget < 0.1f)
             {
@@ -218,6 +219,10 @@ public class BossController : CharacterBase
         if (healthPercentage <= firstWaveSpawnPercent && !firstWaveActivated)
         {
             firstWaveAdds.SetActive(true);
+            foreach (var charBase in firstWaveAdds.GetComponentsInChildren<CharacterBase>())
+            {
+                charBase.OnDeath += Destroy;
+            }
             firstWaveActivated = true;
         }
 
